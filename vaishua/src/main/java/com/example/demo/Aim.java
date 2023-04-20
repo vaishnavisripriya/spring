8945c64bc02d82026cb474f81f68ab76e6722f8a
vaishnavisripriya <113413072+vaishnavisripriya@users.noreply.github.com>
2023-04-20 15:49:40 +0530

    updated  program

---
+++ vaishua/src/main/java/com/example/demo/Aim.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Aim {
+	@Value("${sname:vaishnavi}")
+	private String name;
+	@Value("simple")
+	private String code;
+	String vaish="priya" ;
+	@RequestMapping("/")
+	@ResponseBody
+	public String priya() {
+		return "hi" +"<br>"+ name +"<br>"+ code +vaish;
+	}
+
+}
