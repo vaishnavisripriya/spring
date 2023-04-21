d4b2f6a4ab436cb618fa15f7b35897a8f9b7b1f7
vaishnavisripriya <113413072+vaishnavisripriya@users.noreply.github.com>
2023-04-21 12:01:15 +0530

    day1
    update

--- day1/src/main/java/com/example/demo/Day1Application.java
+++ day1/src/main/java/com/example/demo/Day1Application.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootApplication
 public class Day1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonProcessingException {
 		SpringApplication.run(Day1Application.class, args);
+		
+		Demo s=new Demo(1,"Sri",20);
+		ObjectMapper obj=new ObjectMapper();
+		String str =obj.writeValueAsString(s);
+		System.out.println(str);
 	}
 
 }
